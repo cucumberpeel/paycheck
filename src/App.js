7ff2c28cbd961b7b88816ef7521b7d062304c0c5
cucumberpeel <amorygao@gmail.com>
2024-06-27 19:43:36 -0400

    react setup

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hey there</h1>
+        <h3>Let's get started. You can always change your answers later.</h3>
       </header>
+      <form action={savePrefs}>
+          <label>
+            Including registration, hotels, and transportation, I think a competition should cost no more than <input name="myBudget" type="number" min="100" step="50" defaultValue="100" /> dollars in total.
+          </label>
+          <br />
+          <label>
+            I would like to travel no more than <input name="myTravelTime" type="number" min="0.5" step="0.5" defaultValue="0.5" /> hours one-way to a competition.
+          </label>
+          <br />
+          <button type="submit">Save</button>
+      </form>
     </div>
   );
+}
+
+function savePrefs(formData) {
+  const budget = formData.get("myBudget");
+  const travelTime = formData.get("myTravelTime");
 }
 
 export default App;
